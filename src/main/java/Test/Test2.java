a06475770db07c1edcca756896b2e7b1e5368d13
companyWyz <603948261@qq.com>
2018-09-29 15:03:06 +0800

    2222

--- src/main/java/Test/Test2.java
+++ src/main/java/Test/Test2.java
@@ -4,4 +4,8 @@ public class Test2 {
     public void sout(){
         System.out.println("我只是想测试能不能提交");
     }
+
+    public void testUserName(){
+        System.out.println("测试提交时的用户名");
+    }
 }
